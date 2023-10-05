83f2d7fd75d40969a0a6b882de12cff29c3271bc
Rob J <robert.jahn@advisorsexcel.com>
2023-10-04 19:56:19 -0500

    updates

--- server/data.js
+++ server/data.js
@@ -76,7 +76,134 @@ var data = [
         ]
     },
     {
-        "_id": "005",
+        "_id": "006",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/williams.jpg",
+        "name": "Ted Williams",
+        "about": "Theodore Samuel Williams was an American professional baseball player and manager. He played his entire 19-year Major League Baseball career, primarily as a left fielder, for the Boston Red Sox from 1939 to 1960; his career was interrupted by military service during World War II and the Korean War.",
+        "tags": [
+            "baseball",
+            "mlb",
+            "red sox",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "007",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/ruth.jpg",
+        "name": "Babe Ruth",
+        "about": "George Herman 'Babe' Ruth (February 6, 1895 – August 16, 1948) was an American professional baseball player whose career in Major League Baseball (MLB) spanned 22 seasons, from 1914 through 1935. Nicknamed 'the Bambino' and 'the Sultan of Swat', he began his MLB career as a star left-handed pitcher for the Boston Red Sox, but achieved his greatest fame as a slugging outfielder for the New York Yankees.",
+        "tags": [
+            "baseball",
+            "mlb",
+            "yankees",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "008",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/fisk.jpg",
+        "name": "Carlton Fisk",
+        "about": "Carlton Ernest Fisk (born December 26, 1947),[1] nicknamed 'Pudge' and 'The Commander', is an American former professional baseball player.",
+        "tags": [
+            "baseball",
+            "mlb",
+            "red sox",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "009",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/oliva.jpg",
+        "name": "Tony Oliva",
+        "about": "Tony Pedro Oliva is a Cuban former professional baseball player and coach. He played his entire career in Major League Baseball as a right fielder and designated hitter for the Minnesota Twins from 1962 to 1976.",
+        "tags": [
+            "baseball",
+            "mlb",
+            "twins",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "010",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/carew.jpg",
+        "name": "Rod Carew",
+        "about": "Rodney Cline Carew is a Panamanian former professional baseball player and coach. He played in Major League Baseball as a first baseman, second baseman and designated hitter from 1967 to 1985 for the Minnesota Twins and the California Angels.",
+        "tags": [
+            "baseball",
+            "mlb",
+            "angels",
+            "twins",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "011",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/favre.jpg",
+        "name": "Brett Favre",
+        "about": "Brett Lorenzo Favre is a former American football quarterback who played in the National Football League for 20 seasons, primarily with the Green Bay Packers. Favre had 321 consecutive starts from 1992 to 2010, including 297 regular season games, the most in league history.",
+        "tags": [
+            "football",
+            "nfl",
+            "packers",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "012",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/sanders.jpg",
+        "name": "Barry Sanders",
+        "about": "Barry David Sanders is an American former football running back who played for the Detroit Lions of the National Football League for 10 seasons.",
+        "tags": [
+            "football",
+            "nfl",
+            "lions",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "013",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/bryce.jpg",
+        "name": "Bryce Harper",
+        "about": "Bryce Aron Max Harper is an American professional baseball right fielder and first baseman for the Philadelphia Phillies of Major League Baseball.",
+        "tags": [
+            "baseball",
+            "mlb",
+            "phillies",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "014",
+        "isActive": "true",
+        "age": "29",
+        "picture": "/img/athletes/thome.png",
+        "name": "Jim Thome",
+        "about": "James Howard Thome is an American former professional baseball corner infielder and designated hitter, who played in Major League Baseball for 22 seasons. ",
+        "tags": [
+            "baseball",
+            "mlb",
+            "indians",
+            "mvp"
+        ]
+    },
+    {
+        "_id": "015",
         "isActive": "true",
         "age": "29",
         "picture": "/img/athletes/ohtani.png",
@@ -90,212 +217,58 @@ var data = [
         ]
     },
     {
-        "_id": "005",
+        "_id": "016",
         "isActive": "true",
         "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
+        "picture": "/img/athletes/griffey.jpg",
+        "name": "Ken Griffey",
+        "about": "is an American former professional baseball outfielder who played 22 years in Major League Baseball. He spent most of his career with the Seattle Mariners and Cincinnati Reds, along with a short stint with the Chicago White Sox.",
         "tags": [
             "baseball",
             "mlb",
-            "angels",
+            "mariners",
             "mvp"
         ]
     },
     {
-        "_id": "005",
+        "_id": "017",
         "isActive": "true",
         "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
+        "picture": "/img/athletes/larkin.jpg",
+        "name": "Barry Larkin",
+        "about": "Barry Louis Larkin is an American former professional baseball player. He played shortstop for the Cincinnati Reds of Major League Baseball from 1986 to 2004.",
         "tags": [
             "baseball",
             "mlb",
-            "angels",
+            "reds",
             "mvp"
         ]
     },
     {
-        "_id": "005",
+        "_id": "018",
         "isActive": "true",
         "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
+        "picture": "/img/athletes/cal.jpg",
+        "name": "Cal Ripken Jr",
+        "about": "Calvin Edwin Ripken Jr., nicknamed 'The Iron Man', is an American former baseball shortstop and third baseman who played his entire 21-season career in Major League Baseball for the Baltimore Orioles.",
         "tags": [
             "baseball",
             "mlb",
-            "angels",
+            "orioles",
             "mvp"
         ]
     },
     {
-        "_id": "005",
+        "_id": "019",
         "isActive": "true",
         "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
+        "picture": "/img/athletes/billy.jpg",
+        "name": "Billy Ripken",
+        "about": "William Oliver Ripken, nicknamed Billy the Kid, is an American former professional baseball infielder. He played in Major League Baseball from 1987–1998 for the Baltimore Orioles, Texas Rangers, Cleveland Indians, and Detroit Tigers.",
         "tags": [
             "baseball",
             "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
-            "mvp"
-        ]
-    },
-    {
-        "_id": "005",
-        "isActive": "true",
-        "age": "29",
-        "picture": "/img/athletes/ohtani.png",
-        "name": "Shohei Ohtani",
-        "about": "Shohei Ohtani, nicknamed 'Shotime', is a Japanese professional baseball pitcher and designated hitter for the Los Angeles Angels of Major League Baseball.",
-        "tags": [
-            "baseball",
-            "mlb",
-            "angels",
+            "orioles",
             "mvp"
         ]
     }
